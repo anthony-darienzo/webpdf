762be379cc3665000767c6b4ec6a0e0c7637487f
Anthony D'Arienzo <anthony384@outlook.com>
2024-10-09 00:02:22 -0500

    Added a rudimentary pdf.js wrapper using Scotty.

--- app/Main.hs
+++ app/Main.hs
@@ -1,21 +1,26 @@
-{-# LANGUAGE OverloadedStrings     #-}
-{-# LANGUAGE QuasiQuotes           #-}
-{-# LANGUAGE TemplateHaskell       #-}
-{-# LANGUAGE TypeFamilies          #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
 module Main where
 
-import           Yesod
+import Web.Scotty
 
-data HelloWorld = HelloWorld
+import Network.Wai.Middleware.StaticEmbedded
 
-mkYesod "HelloWorld" [parseRoutes|
-/ HomeR GET
-|]
+import Data.ByteString (ByteString)
+import Data.FileEmbed (makeRelativeToProject, embedDir)
+import System.FilePath ( (</>) )
 
-instance Yesod HelloWorld
-
-getHomeR :: Handler Html
-getHomeR = defaultLayout [whamlet|Hello World!|]
+grabEmbeddedFiles :: [(FilePath, ByteString)]
+grabEmbeddedFiles =
+  let xs  = $(makeRelativeToProject "web" >>= embedDir)
+      ys  = $(makeRelativeToProject "pdfjs-dist" >>= embedDir)
+      xs' = map (\ (f,b) -> ("web" </> f, b)) xs
+      ys' = map (\ (f,b) -> ("pdfjs-dist" </> f, b)) ys
+  in xs' <> ys'
 
 main :: IO ()
-main = warp 3000 HelloWorld
+main = scotty 3000 $ do
+  middleware (static grabEmbeddedFiles)
+  get "/pdf" $ do
+    html "<h1>PDF viewer in progress.</h1>"
+    
